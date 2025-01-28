d9d47443a966f5650977b2304a1e8dbc53b5f033
Roberto Agredo <memoriaderam@gmail.com>
2025-01-28 01:22:28 -0400

    Update home.jsx
    Update home.jsx

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,138 @@
-import React from "react";
-
+import React, {createRef, useEffect, useState} from "react";
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import rigoImage from "../../img/task.jpg";
+
+const inicialTask = {
+	label:"",
+	is_done: false
+}
+
+const urlBase = "https://playground.4geeks.com/todo"
 
 //create your first component
 const Home = () => {
+    const [task, settask] = useState(inicialTask)
+	const [taskList, setTaksList] = useState ([])
+
+    // function that add to white in the input y push in the useState (task)
+    const handleChange = ({target}) => {
+          settask ({
+			   ...task,
+			   [target.name]: target.value
+		  })
+	}
+
+    const getAllTask = async () => {
+		try{
+             const respond = await fetch(`${urlBase}/users/roberto`)
+			 const data = await respond.json()
+			 if (respond.ok) {
+                    setTaksList(data.todos)
+					 } else {
+					createNewUser()
+					 }	
+					
+			}   catch {error} {
+			console.log(error)
+		}
+	}
+			 
+    //function to creater user
+	const createNewUser = async () => {
+          try {
+         const respond  = await fetch(`${urlBase}/users/roberto`,{
+			method:"POST"
+		 })
+		 //const data = await respond.json()
+		  } catch(error) {
+			console.log(error)
+		  }
+	}
+	
+	// function to create new task 
+	const addTask = async (event) => {
+		
+		if (event.key == "Enter"){
+		try{
+              const respond = await  fetch (`${urlBase}/todos/roberto`,{
+				method: "POST",
+				headers:{
+					"Content-Type": "application/json"
+				}, 
+				body: JSON.stringify(task) 
+			  })
+			  if(respond.ok){
+				getAllTask()
+			  }
+		}	catch (error){
+            console.log(error)
+		}
+		}
+	}
+    
+
+    const deleteTask = async (id) => {
+        try {
+			const respond = await fetch(`${urlBase}/todos/${id}`, {
+                method: "DELETE"
+			})
+			if(respond.ok){
+				getAllTask()
+			  }
+
+		} catch(error) {
+			console.log(error)
+		}
+
+	}
+
+	useEffect (()=> {
+		      getAllTask()
+	})
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		
+		<div className="container text-center">
+			    <p>
+				  <img src={rigoImage} />
+			    </p>
+
+			<div className="text-center row">
+				<div className="text-center col-12 col-md-7">
+			      <h1 className="">List of task.</h1>
+				  <form onSubmit={(event) => event.preventDefault()}>
+					<input
+					type="text"
+					placeholder="Add a new task"
+					className="form-control"
+					name="label"
+					value={task.label}
+					onChange={handleChange}
+					onKeyDown={addTask}
+
+					/>
+				  </form>
+
+				  {
+                    taskList.length <= 0 ? <div> no tiene tareas</div>:       
+
+					     taskList.map((item) => (
+						    <div key={item.id} className="class">
+							      {item.label}
+							 <span><button OnClick= { ( ) => deleteTask (item.id) } > x </button>
+							 
+							 </span>
+						    </div>
+						))
+				  }
+				
+			   
+			      
+				</div>
+			</div>
 		</div>
 	);
 };
 
 export default Home;
+
